d52762d54fa88f7a34afbc88d70ca3aa7ce088b1
xiangnan <280145668@qq.com>
2024-02-21 10:13:12 +0800

    test3

--- scripts/prepublish.mjs
+++ scripts/prepublish.mjs
@@ -9,7 +9,7 @@ let { version } = JSON.parse(await fs.readFile('./package.json'))
 const playgroundDir = path.resolve(__dirname, '../playground/')
 cd(playgroundDir)
 
-await $`pnpm install`
+// await $`pnpm install`
 await $`git add -A .`
 try {
   await $`git commit -m "version ${version} snapshot"`
@@ -20,10 +20,10 @@ try {
 }
 
 await $`git tag -m "v${version}" v${version}`
-await $`git push --follow-tags`
+await $`git push`
 
 const projectRoot = path.resolve(__dirname, '../')
 cd(projectRoot)
 await $`git add playground`
 await $`git commit -m 'chore: update snapshot' --allow-empty`
-await $`git push --follow-tags`
+await $`git push`
